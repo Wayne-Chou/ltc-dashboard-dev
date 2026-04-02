32c7ab741632b60e5ebd26d12305fce47b0e564b
hank <lohaslife.hank@gmail.com>
2026-04-02 15:56:54 +0800

    移除dist資料夾

--- dist/js/common/config.js
+++
@@ -1,5 +0,0 @@
-// config.js
-// 全站共用設定
-window.APP_CONFIG = {
-  BASE_URL: "https://service.fongai.co/WebAPI/api",
-};
--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 import { resolve } from "path";
 
 export default defineConfig({
-  base: "/dashboard/",
+  base: "/ltc-dashboard-dev/",
   build: {
     assetsInlineLimit: 0,
     rollupOptions: {
